461a448f7a78432181326e73da98e005bdb13f66
fmg1395 <franes9514@gmail.com>
2020-03-20 14:38:54 -0600

    modificacion de modelo en progreso

--- proyecto/src/java/proyecto/modelo/modelo.java
+++ proyecto/src/java/proyecto/modelo/modelo.java
@@ -6,5 +6,22 @@ package proyecto.modelo;
  * @author frank
  */
 public class modelo {
+
+    public modelo() {
+        cliente = null;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+   
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
     
+    
+    
+    
+    private Cliente cliente;
 }
